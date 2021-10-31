57c0a8b809433e41bde1f330075bdc82c2fffa35
isaac long <isaaclong123@outlook.com>
2021-10-31 18:14:07 -0400

    first commit

---
+++ models/Workout.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+const ExerciseSchema = new Schema({
+
+    type: String,
+        name: String,
+        duration: Number,
+        weight: Number,
+        reps: Number,
+        sets: Number,
+    
+})
+const WorkoutSchema = new Schema({
+
+    day:{
+        type: Date,
+        required: true,
+    },
+    exercises:[ExerciseSchema],
+    
+    
+})
+
+
+const Workout = mongoose.model("Workout", WorkoutSchema)
+
+
+module.exports= Workout;
